969cab41fdc5330b3b0d1921eedc3bb8e5f80077
Ian Matyssik <ian.matyssik@gmail.com>
2015-07-31 13:57:15 +0900

    Laying out the class for the module, ES6 it is

--- index.js
+++ index.js
@@ -1,4 +1,38 @@
 var bn = require('bn.js'),
-    hashids = require('hashids');
+    Hashids = require('hashids');
 
+class ShortDUID {
+
+    constructor(shard_id, salt, epoch_start) {
+        this.shard_id = new bn(shard_id, 10);
+        this.salt = salt;
+        this.epoch_start = new bn(epoch_start, 10);
+
+        this.drift = 0;
+        this.hashids = new Hashids(this.salt);
+    }
+
+    getDUID(count) {
+    }
+
+    getDUIDInt(count) {
+    }
+
+    getShardID() {
+    }
+
+    getEpochStart() {
+    }
+
+    getSalt() {
+    }
+
+    getCurrentTimeMs() {
+    }
+
+    driftTime(milliseconds) {
+        this.drift = milliseconds;
+    }
+
+}
 exports.init = short_duid.ShortDUID;
