5dc048dfd7b47a8e297d96290dcacf52d737feb4
Ian Matyssik <ian.matyssik@gmail.com>
2015-08-09 12:24:56 +0900

    Getting there, just layed out skeleton with mocks

--- index.js
+++ index.js
@@ -1,67 +1,59 @@
 'use strict';
-var bn = require('bn.js'),
+var BN = require('bn.js'),
     Hashids = require('hashids');
 
-class ShortDUID {
-
-    constructor(shard_id, salt, epoch_start) {
-        this.shard_id = new bn(shard_id, 10);
-        this.salt = salt;
-        this.epoch_start = new bn(epoch_start, 10);
-
-        this.drift = 0;
-        this.hashids = new Hashids(this.salt);
-
-        this.sequence_ = 0;
-        this.ts_sequence_ = [];
-
-    }
-
-    getDUID(count) {
-        let tmp = [];
-        for(let i = 0; i < count; i++) {
-            tmp.push(this._getNextSequence());
-        }
-        return tmp;
-    }
-
-    getDUIDInt(count) {
-        let tmp = [];
-        for(let i = 0; i < count; i++) {
-            tmp.push(this._getNextSequence());
-        }
-        return tmp;
-    }
-
-    getShardID() {
-    }
-
-    getEpochStart() {
-    }
-
-    getSalt() {
-    }
-
-    getCurrentTimeMs() {
-    }
-
-    driftTime(milliseconds) {
-        this.drift = milliseconds;
-    }
-
-    *_getAssembledDUID(count) {
-        for(let i = 0; i < count; i++) {
-            yield 1;
-        }
-    }
-
-    _getNextSequence() {
-        return this.sequence_++;
-    }
-
-    _getOurMillisFromEpoch() {
-        return (new Date).now();
-    }
-
-}
 exports.init = ShortDUID;
+
+function ShortDUID(shard_id, salt, epoch_start) {
+    if (!(this instanceof ShortDUID)) return new ShortDUID(shard_id, salt, epoch_start);
+    var duid = this;
+
+    duid.shard_id = new BN(shard_id, 10);
+    duid.salt = salt;
+
+    var now = new BN(+new Date, 10);
+    duid.epoch_start = new BN(epoch_start, 10);
+    if(now.cmp( duid.epoch_start ) === -1) {
+        duid.epoch_start = new BN(0, 10);
+    }
+
+    duid.time_drift = 0;
+    duid.hashids = new Hashids(duid.salt);
+
+    duid.sequence = new BN(0, 10);
+    duid.ts_sequence = [];
+
+    return duid;
+};
+
+ShortDUID.prototype.getDUID = function(count) {
+    var duid = this;
+
+    return [1];
+};
+
+ShortDUID.prototype.getDUIDInt = function(count) {
+    var duid = this;
+
+    return [1];
+};
+
+ShortDUID.prototype.getShardID = function() {
+    return this.shard_id.toString(10) * 1;
+};
+
+ShortDUID.prototype.getEpochStart = function() {
+    return this.epoch_start.toString(10);
+};
+
+ShortDUID.prototype.getSalt = function() {
+    return this.salt;
+};
+
+ShortDUID.prototype.getCurrentTimeMs = function() {
+    return +new Date;
+};
+
+ShortDUID.prototype.driftTime = function() {
+    return this.time_drift;
+};
